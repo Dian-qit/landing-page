e21bd66f34f23f066623823fd46d396adab3c32c
Vincent Adrian <vincentquitoriano38@gmail.com>
2024-12-16 01:37:42 +0800

    page set up

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,44 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-// Home Page Route
-router.get('/', (req, res) => {
-    res.render('index', { 
-        title: 'Home',
-        description: 'Welcome to our landing page'
-    });
+router.get('', (req, res) => {
+    const locals = {
+        title: "Renew Homes",
+        description: "Wawa"
+    }
+
+    res.render('index', { locals });
 });
 
-// About Page Route
-router.get('/about', (req, res) => {
-    res.render('about', { 
-        title: 'About Us',
-        description: 'Learn more about our company'
-    });
+
+router.get('/contact-us', (req, res) => {
+    res.render('contact-us');
 });
 
-// Contact Page Route
-router.get('/contact', (req, res) => {
-    res.render('contact', { 
-        title: 'Contact',
-        description: 'Get in touch with us'
-    });
-});
-
-// Services Page Route
-router.get('/services', (req, res) => {
-    res.render('services', { 
-        title: 'Our Services',
-        description: 'Explore the services we offer'
-    });
-});
-
-// Error 404 Route (place this last)
-router.use((req, res) => {
-    res.status(404).render('404', { 
-        title: 'Page Not Found',
-        description: 'The page you are looking for does not exist'
-    });
-});
 
 module.exports = router;
